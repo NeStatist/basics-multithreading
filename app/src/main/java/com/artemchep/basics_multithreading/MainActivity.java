22ba61c7fe0365505983a18d9b9032fbe9ff72ab
NeStatist <doroshkor@gmail.com>
2019-08-19 17:18:11 +0300

    solution

--- app/src/main/java/com/artemchep/basics_multithreading/MainActivity.java
+++ app/src/main/java/com/artemchep/basics_multithreading/MainActivity.java
@@ -1,10 +1,11 @@
 package com.artemchep.basics_multithreading;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import androidx.annotation.UiThread;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,13 +15,28 @@ import com.artemchep.basics_multithreading.domain.Message;
 import com.artemchep.basics_multithreading.domain.WithMillis;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<WithMillis<Message>> mList = new ArrayList<>();
 
     private MessageAdapter mAdapter = new MessageAdapter(mList);
+
+    private Queue<WithMillis<Message>> queue;
+    private boolean tempRun = true;
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(android.os.Message msg) {
+
+            WithMillis<Message> messageWithMillis = (WithMillis<Message>) msg.obj;
+            update(new WithMillis<>(messageWithMillis.value.copy(messageWithMillis.value.cipherText),
+                    (messageWithMillis.elapsedMillis)));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +47,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(mAdapter);
 
-        showWelcomeDialog();
-    }
+        queue = new LinkedList<>();
 
-    private void showWelcomeDialog() {
-        new AlertDialog.Builder(this)
-                .setMessage("What are you going to need for this task: Thread, Handler.\n" +
-                        "\n" +
-                        "1. The main thread should never be blocked.\n" +
-                        "2. Messages should be processed sequentially.\n" +
-                        "3. The elapsed time SHOULD include the time message spent in the queue.")
-                .show();
+        myThreed();
+
     }
 
     public void onPushBtnClick(View view) {
@@ -54,19 +63,13 @@ public class MainActivity extends AppCompatActivity {
         mList.add(message);
         mAdapter.notifyItemInserted(mList.size() - 1);
 
-        // TODO: Start processing the message (please use CipherUtil#encrypt(...)) here.
-        //       After it has been processed, send it to the #update(...) method.
+        synchronized (queue) {
+            queue.add(new WithMillis<>(message.value, System.currentTimeMillis()));
+            queue.notifyAll();
+        }
 
-        // How it should look for the end user? Uncomment if you want to see. Please note that
-        // you should not use poor decor view to send messages to UI thread.
-//        getWindow().getDecorView().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                final Message messageNew = message.value.copy("sample :)");
-//                final WithMillis<Message> messageNewWithMillis = new WithMillis<>(messageNew, CipherUtil.WORK_MILLIS);
-//                update(messageNewWithMillis);
-//            }
-//        }, CipherUtil.WORK_MILLIS);
+        update(message);
+
     }
 
     @UiThread
@@ -82,4 +85,41 @@ public class MainActivity extends AppCompatActivity {
         throw new IllegalStateException();
     }
 
+    public void myThreed(){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(tempRun) {
+                    List<WithMillis<Message>> messageArray;
+
+                    synchronized (queue) {
+                        messageArray = new ArrayList<>(queue);
+                        queue.clear();
+                    }
+
+                    for(int i = 0; i < messageArray.size(); i++) {
+                        android.os.Message message = android.os.Message.obtain();
+                        WithMillis<Message> messageWithMillis = messageArray.get(i);
+                        message.obj = new WithMillis<>(messageWithMillis.value.copy
+                                (CipherUtil.encrypt(messageWithMillis.value.plainText)), System.currentTimeMillis() - messageWithMillis.elapsedMillis);
+                        message.setTarget(handler);
+                        message.sendToTarget();
+                    }
+
+                    synchronized (queue) {
+                        if(queue.isEmpty()) {
+                            try {
+                                queue.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
 }
+
